95e556addc06e9bcf14223e095a048f1e34ee078
Serg <serj.nights@live.com>
2016-09-23 12:26:10 -0400

    favicon added

--- streamviewer.js
+++ streamviewer.js
@@ -1,7 +1,9 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var favicon = require('serve-favicon');
 
 var app = connect();
 
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(serveStatic(__dirname+'/public', {'index': ['index.html']}));
 app.listen(3002);
